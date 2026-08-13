c12a651b326844a2ef1499a30ba1fa10a86caa34
arisson2001rojas-design <290829377+arisson2001rojas-design@users.noreply.github.com>
2026-08-13 16:16:59 -0600

    fix: update browser companion localhost port

--- browser-companion/popup.js
+++ browser-companion/popup.js
@@ -41,5 +41,5 @@ radarNotifications.addEventListener("change", () => {
 });
 
 document.getElementById("openPriceBuddy").addEventListener("click", () => {
-  chrome.tabs.create({ url: "http://127.0.0.1:8281/admin/deal-offers" });
+  chrome.tabs.create({ url: "http://127.0.0.1:8381/admin/deal-offers" });
 });
